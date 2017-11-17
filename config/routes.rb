d878cf11718f27503f11b9763f2e64a6c5b47ede
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-17 13:27:21 +0900

    add static page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_page/help'
 
-  get 'static_page/contact'
+  root :to => redirect('static_page/help')
+
+  # static page
+  get 'static_page/help', to: 'static_page#help'
+  get 'static_page/contact', to: 'static_page#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
