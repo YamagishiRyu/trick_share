b8659d800cf1d08cf0f282746d98e1126dcc34d3
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-17 10:53:42 +0900

    add contributor model

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,5 +1,7 @@
 class Contributor < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validtes :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validtes :nick_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :nick_name, presence: true, length: {maximum: 30 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6}
 end
