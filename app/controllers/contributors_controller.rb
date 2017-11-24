69490e72a93fdbfd66dda20d1ada3bf082ec9142
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-24 15:18:18 +0900

    add create method

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,8 +1,23 @@
 class ContributorsController < ApplicationController
   def new
+    @contributor = Contributor.new
   end
 
   def show 
     @contributor = Contributor.find(params[:id])
   end
+
+  def create
+    @contributor = Contributor.new(user_params)
+    if @contributor.save
+      # success
+    else
+      render 'new'
+    end
+  end
+
+  private 
+    def user_params
+      params.require(:contributor).permit(:nick_name, :email, :password, :password_confirmation)
+    end
 end
