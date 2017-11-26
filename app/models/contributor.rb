3caceee2c2e0bffc3dbd75315f695d190300ef07
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-26 20:57:24 +0900

    add session function

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,7 +1,32 @@
 class Contributor < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :nick_name, presence: true, length: {maximum: 30 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6}
+
+  def Contributor.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def Contributor.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = Contributor.new_token
+    update_attribute(:remember_digest, Contributor.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
