39d274bb8db20fae0267ae702cb02fcea9538bf8
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-30 22:59:58 +0900

    move save tag function for model

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -15,4 +15,21 @@ class Trick < ApplicationRecord
   validates :content, presence: true
   validates :title, presence: true
   validates :duration, presence: true, numericality: :only_integer
+
+  def save_tags(tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # destroy old taggings
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name: old_name)
+    end
+
+    # create new taggings
+    new_tags.each do |new_name|
+      tagging_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << tagging_tag
+    end
+  end
 end
