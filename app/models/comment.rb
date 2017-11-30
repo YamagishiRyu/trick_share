dcadc2d69f6f433b21322f8c2385d0650c0525a5
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-30 15:59:07 +0900

    add validation and association to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+  # association
+  belongs_to :contributor
+  belongs_to :trick
+
+  # order
+  default_scope -> { order(created_at: :desc) }
+
+  # validation
+  validates :contributor_id, presence: true
+  validates :trick_id, presence: true
+  validates :content, presence: true
 end
