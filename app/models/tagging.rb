86772bfeff5ff9b9a0622d3b76231b4e40412bc0
YamagishiRyu <bubusu.ryu@gmail.com>
2017-11-30 21:27:32 +0900

    add validation to tagging:

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,2 +1,9 @@
 class Tagging < ApplicationRecord
+  # association
+  belongs_to :trick
+  belongs_to :tag
+
+  #validation
+  validates :trick_id, presence: true
+  validates :tag_id, presence: true
 end
